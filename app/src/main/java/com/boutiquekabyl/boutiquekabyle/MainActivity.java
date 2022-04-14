f2b3c98c5c0fff91d99e6059175a636a97db8fd4
Mohand Agawaw <arabmohand6@gmail.com>
2022-04-14 19:55:41 +0300

    Boutique Kabyle Mobile

---
+++ app/src/main/java/com/boutiquekabyl/boutiquekabyle/MainActivity.java
@@ -0,0 +1,36 @@
+package com.boutiquekabyl.boutiquekabyle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mywebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebView=(WebView) findViewById(R.id.mwb);
+        mywebView.setWebViewClient(new WebViewClient());
+        mywebView.loadUrl("https://boutique-kabyle.org/");
+        WebSettings webSettings=mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+    public class mywebClient extends WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon){
+            super.onPageStarted(view,url,favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view,String url){
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+}
